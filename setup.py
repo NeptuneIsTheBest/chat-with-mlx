4cfd2114ad74206789bcdd014b54fe243aa11e7c
Neptune <34836531+NeptuneIsTheBest@users.noreply.github.com>
2024-11-25 22:05:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chat-with-mlx-refresh",
-    version="0.1.2",
+    version="0.1.3",
     author="NeptuneIsTheBest",
     author_email="13058097081a@gmail.com",
     description="An all-in-one chat Web UI based on the MLX framework, designed for Apple Silicon.",
