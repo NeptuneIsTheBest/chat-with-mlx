11047ab1596052fb3b3486e7862bb236668ee871
Neptune <34836531+NeptuneIsTheBest@users.noreply.github.com>
2024-12-10 21:14:06 +0800

    feat: OpenAI API models can be added (#17)

--- chat_with_mlx_refresh/model_management.py
+++ chat_with_mlx_refresh/model_management.py
@@ -5,48 +5,48 @@ from .language import get_text
 from .model import ModelManager
 
 
-class AddModelBlock(ComponentsBlock):
+class AddLocalModelBlock(ComponentsBlock):
     def __init__(self, model_manager: ModelManager):
         self.model_manager = model_manager
 
         self.model_name_textbox = gr.Textbox(
-            label=get_text("Page.ModelManagement.AddModelBlock.Textbox.model_name.label"),
-            placeholder=get_text("Page.ModelManagement.AddModelBlock.Textbox.model_name.placeholder"),
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.model_name.label"),
+            placeholder=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.model_name.placeholder"),
             interactive=True
         )
         self.original_repo_textbox = gr.Textbox(
-            label=get_text("Page.ModelManagement.AddModelBlock.Textbox.original_repo.label"),
-            placeholder=get_text("Page.ModelManagement.AddModelBlock.Textbox.original_repo.placeholder"),
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.original_repo.label"),
+            placeholder=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.original_repo.placeholder"),
             interactive=True
         )
         self.mlx_repo_textbox = gr.Textbox(
-            label=get_text("Page.ModelManagement.AddModelBlock.Textbox.mlx_repo.label"),
-            placeholder=get_text("Page.ModelManagement.AddModelBlock.Textbox.mlx_repo.placeholder"),
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.mlx_repo.label"),
+            placeholder=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.mlx_repo.placeholder"),
             interactive=True
         )
         self.quantize_dropdown = gr.Dropdown(
-            label=get_text("Page.ModelManagement.AddModelBlock.Dropdown.quantize.label"),
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Dropdown.quantize.label"),
             choices=["None", "4bit", "8bit", "bf16", "bf32"],
             value="None",
             interactive=True
         )
         self.default_language_dropdown = gr.Dropdown(
-            label=get_text("Page.ModelManagement.AddModelBlock.Dropdown.default_language.label"),
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Dropdown.default_language.label"),
             choices=["multi"],
             interactive=True
         )
         self.default_system_prompt_textbox = gr.Textbox(
-            label=get_text("Page.ModelManagement.AddModelBlock.Textbox.default_system_prompt.label"),
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.default_system_prompt.label"),
             interactive=True
         )
         self.multimodal_ability_dropdown = gr.Dropdown(
-            label=get_text("Page.ModelManagement.AddModelBlock.Dropdown.multimodal_ability.label"),
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Dropdown.multimodal_ability.label"),
             choices=["None", "vision"],
             value="None",
             multiselect=True
         )
         self.add_button = gr.Button(
-            value=get_text("Page.ModelManagement.AddModelBlock.Button.add.value")
+            value=get_text("Page.ModelManagement.AddLocalModelBlock.Button.add.value")
         )
 
     def render_all(self):
@@ -57,4 +57,44 @@ class AddModelBlock(ComponentsBlock):
         self.default_language_dropdown.render()
         self.default_system_prompt_textbox.render()
         self.multimodal_ability_dropdown.render()
+        self.add_button.render()
+
+
+class AddAPIModelBlock(ComponentsBlock):
+    def __init__(self, model_manager: ModelManager):
+        self.model_manager = model_manager
+
+        self.model_name_textbox = gr.Textbox(
+            label=get_text("Page.ModelManagement.AddAPIModelBlock.Textbox.model_name.label"),
+            placeholder=get_text("Page.ModelManagement.AddAPIModelBlock.Textbox.model_name.placeholder"),
+            interactive=True
+        )
+        self.nick_name_textbox = gr.Textbox(
+            label=get_text("Page.ModelManagement.AddAPIModelBlock.Textbox.nick_name.label"),
+            interactive=True
+        )
+        self.api_key_textbox = gr.Textbox(
+            label=get_text("Page.ModelManagement.AddAPIModelBlock.Textbox.api_key.label"),
+            placeholder=get_text("Page.ModelManagement.AddAPIModelBlock.Textbox.api_key.placeholder"),
+            interactive=True
+        )
+        self.base_url_textbox = gr.Textbox(
+            label=get_text("Page.ModelManagement.AddAPIModelBlock.Textbox.base_url.label"),
+            placeholder=get_text("Page.ModelManagement.AddAPIModelBlock.Textbox.base_url.placeholder"),
+            interactive=True
+        )
+        self.default_system_prompt_textbox = gr.Textbox(
+            label=get_text("Page.ModelManagement.AddLocalModelBlock.Textbox.default_system_prompt.label"),
+            interactive=True
+        )
+        self.add_button = gr.Button(
+            value=get_text("Page.ModelManagement.AddLocalModelBlock.Button.add.value")
+        )
+
+    def render_all(self):
+        self.model_name_textbox.render()
+        self.nick_name_textbox.render()
+        self.api_key_textbox.render()
+        self.base_url_textbox.render()
+        self.default_system_prompt_textbox.render()
         self.add_button.render()
